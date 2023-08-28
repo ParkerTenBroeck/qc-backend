6536f4d0ce688972238f14c7fc6aa2bf3b469b34
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2023-08-28 11:42:10 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,11 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   networking.hostName = "qc-server"; # Define your hostname.
   networking.interfaces.eno1.ipv4.addresses = [ {
     address = "192.168.1.80";
