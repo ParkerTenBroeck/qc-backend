b979ac36f2a56b280e8ed7132a20f2acb8478d7e
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2023-08-28 10:24:37 -0400

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,175 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "qc-server"; # Define your hostname.
+  networking.interfaces.eno1.ipv4.addresses = [ {
+    address = "192.168.1.80";
+    prefixLength = 24;
+  } ];
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [ "192.168.1.1" ];
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  systemd.services.qc-server = {
+    script = ''
+      export PATH=/run/wrappers/bin:/home/rct/.nix-profile/bin:/etc/profiles/per-user/rct/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin
+      sleep 5 || true
+      
+      rm /home/rct/qc-logs.sh || true
+      su - rct -c "echo journalctl -xefu qc-server > ~/qc-logs.sh;chmod +x ~/qc-logs.sh" || true
+
+      rm /home/rct/restart-qc.sh || true
+      su - rct -c "echo sudo systemctl restart qc-server.service > restart-qc.sh;chmod +x ~/restart-qc.sh" || true
+
+      su - rct -c "cd ~;git clone https://github.com/ParkerTenBroeck/qc-backend.git" || true
+      
+      rm /home/rct/hard-update.sh || true
+      su - rct -c "ln -s ~/qc-backend/hard-update.sh ~/hard-update.sh;chmod +x ~/hard-update.sh" || true
+      
+      rm /home/rct/update.sh || true
+      su - rct -c "ln -s ~/qc-backend/update.sh ~/update.sh;chmod +x ~/update.sh" || true
+      
+      su - rct -c "chmod +x ~/qc-backend/run-service.sh" || true
+      su - rct -c "~/qc-backend/run-service.sh"
+    '';
+    wantedBy = ["network-online.target"];
+  };
+
+  # Enable networking
+  networking.networkmanager.enable = true; 
+
+  # Set your time zone.
+  time.timeZone = "America/Toronto";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Cinnamon Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.rct = {
+    isNormalUser = true;
+    description = "rct";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "rct";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  neovim
+  git
+  cargo
+  rustc
+  cmake
+  binutils
+  gcc
+  hstr
+  screen
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 8000 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+}
